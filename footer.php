da256f7d0e9a68892f41d3d93ecd04becdb60993
JeroenZwaal <93530512+JeroenZwaal@users.noreply.github.com>
2022-04-11 09:31:24 +0200

    footer.php done

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
     <div class="footer">
     <p>Winkeltje</p>
-    <p class="copyRight">-Copy Right-</p>
+    <p class="copyRight">@2022 Scouting B.V. - alle rechten voorbehouden</p>
+    <p>Contact - 06 12345678</p>
     
     </div>
 </footer>
