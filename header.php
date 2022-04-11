3a8c9776a67972395aad2d9de6a091e0bea09021
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-04-11 09:58:25 +0200

    header done

--- header.php
+++ header.php
@@ -1,13 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
     <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/36ac186ef8.js" crossorigin="anonymous"></script>
+    <title>Project Winkeltje</title>
 </head>
 <body>
     <header>
-        
-    </header>
+        <div class="wrapper">
+            <div class="header">
+                <div class="logo">
+                    <img src="img/placeholder.jpg" alt="Logo">
+                </div>
+                <nav class="nav">
+                    <a href="">Mannen</a>
+                    <a href="">Vrouwen</a>
+                    <a href="">kinderen</a>
+                </nav>
+                <div class="login">
+                    <i class="fas fa-shopping-cart"></i>
+                    <i class="fas fa-user"></i>
+                    <i class="fas fa-heart"></i>
+                </div>
+            </div>
+        </div>
